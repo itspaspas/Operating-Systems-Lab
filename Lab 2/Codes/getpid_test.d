a08172b7ca9b93c62c04544f627124247397ce03
Majid Sadeghinejad <majid.sadeghinejad@outlook.com>
2025-04-15 21:42:38 +0330

    clean agian

--- Lab 2/Codes/getpid_test.d
+++
@@ -1,1 +0,0 @@
-getpid_test.o: getpid_test.c /usr/include/stdc-predef.h types.h user.h
--- Lab 2/Codes/make_user_test.d
+++
@@ -1,2 +0,0 @@
-make_user_test.o: make_user_test.c /usr/include/stdc-predef.h types.h \
- stat.h user.h fcntl.h
